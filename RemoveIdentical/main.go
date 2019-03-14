7099cf2d9d89e8fc3e26a7df5b4e0b950ce99a68
gespathis <gespathis@gmail.com>
2019-03-15 00:08:18 +0200

    basic channel go routines example

---
+++ RemoveIdentical/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func dataSource(uploadStream chan<- string) {
+
+	for _, v := range []string{"help me take good", "good", "offer from beat", "success beat success"} {
+		uploadStream <- v
+	}
+	close(uploadStream)
+}
+
+func wordSeperator(uploadStream chan<- string, downloadStream <-chan string) {
+
+	for v := range downloadStream {
+		for _, word := range strings.Fields(v) {
+			uploadStream <- word
+		}
+	}
+	close(uploadStream)
+}
+
+func notSameWord(uploadStream chan<- string, downloadStream <-chan string) {
+	previousWord := make(map[string]bool)
+
+	for v := range downloadStream {
+		if !previousWord[v] {
+			previousWord[v] = true
+			uploadStream <- v
+		}
+	}
+	close(uploadStream)
+}
+
+func printWords(downloadStream <-chan string) {
+
+	for {
+		word, ok := <-downloadStream
+
+		if !ok {
+			return
+		}
+		fmt.Printf("the word is : %v \n", word)
+	}
+}
+
+func main() {
+
+	c0 := make(chan string)
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go dataSource(c0)
+	go wordSeperator(c1, c0)
+	go notSameWord(c2, c1)
+	printWords(c2)
+}
